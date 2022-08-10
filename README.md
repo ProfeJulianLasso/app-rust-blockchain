4226eaa55257922555f96ba930d1defcee97415e
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-08-10 00:12:37 -0500

    Soporte para procesos multihilo dependiendo de la cantidad de procesadores disponibles y la carga deseada

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Corazón de sistema blockchain
-Microsistema con base en **Proof of Work** (Prueba de Trabajo) escrito en rust con el fin de crear un sistema capaz de dar respaldo a sistemas más grandes.
+Microsistema con base en **Proof of Work** (Prueba de Trabajo) escrito en rust con el fin de crear un microsistema capaz de dar respaldo a sistemas más grandes.
 
 ## Cómo funciona
 ---
@@ -7,25 +7,40 @@ Al ejecutable por consola se le pasan dos parámetros:
 
 - JSON tipo String con propiedad ```"nonce": null```
 - Nivel de dificultad ```7``` por ejemplo
+- Carga de la CPU ```80``` porciento por ejemplo
 
-```rust
-./app-rust-blockchain "{\"data\": \"Julian Lasso\", \"nonce\": null, \"date\": \"2022-01-01T00:00:00Z\"}" 7
+Crear un bloque
+```shell
+./app-rust-blockchain create "{\"data\": \"Julian Lasso\", \"nonce\": null, \"date\": \"2022-01-01T00:00:00Z\"}" 7 80
+```
+Resultado
+```shell
+Cantidad de CPU usadas: 16 - 80%
+Nivel de dificultadad: 7
+Nonce encontrado: 748833857
+Hash: 0000000a9c3d64fd5b81c2afa51a65c96179ac02381b66b9047653effa57ac02
+Datos resultantes: {"data": "Julian Lasso", "nonce": 748833857, "date": "2022-01-01T00:00:00Z"}
+Tiempo transcurrido: 21s
 ```
 
-## Resltado en consola
----
+Verificar un bloque creado
 ```shell
-Nivel de dificultadad: 7
-Nonce encontrado: 671660449
-Datos resultantes: {"data": "Julian Lasso", "nonce": 671660449, "date": "2022-01-01T00:00:00Z"}
-Hash: 0000000c0b8f03df923acbe02804feb5248021b0bcfe9e5d517cae1f68e5cf81
-Cantidad de intentnos: 250.742.845
-Tiempo transcurrido: 145s
+./app-rust-blockchain check "{\"data\": \"Julian Lasso\", \"nonce\": null, \"date\": \"2022-01-01T00:00:00Z\"}" 748833857 0000000a9c3d64fd5b81c2afa51a65c96179ac02381b66b9047653effa57ac02
+```
+Resultado en caso de éxito
+```shell
+OK
+```
+Resultado en caso de error
+```shell
+ERROR
 ```
 
 ## Información importante
 ---
-No use niveles de dificultad más allá de ```6```, ya que ```7``` es un nivel de difucltad algo elevado que puede llegar a tomar 3 a 5 minutos o más en encontrar una solución dependiendo de su procesador
+No use niveles de dificultad más allá de ```6```, ya que ```7``` es un nivel de difucltad algo elevado que puede llegar a tomar 3 a 5 minutos o más en encontrar una solución dependiendo de su procesador.
+
+Tampoco use más allá del 80% de potencia de su procesador.
 
 ## Librerías
 ---
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,126 @@
 use blake3::hash as blake3;
+use easy_parallel::Parallel;
 use libsodium_sys::randombytes_random;
 use regex::Regex;
 use std::env;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
 use std::time::Instant;
-use thousands::Separable;
+
+static GLOBAL_FLAG: AtomicBool = AtomicBool::new(false);
 
 fn main() {
-    let start = Instant::now();
     let args: Vec<String> = env::args().collect();
 
+    let option = &args[1];
+    match option.as_str() {
+        "create" => {
+            let start = Instant::now();
+            let info = args[2].to_string();
+            let level = args[3].to_string();
+            let stress = args[4].to_string();
+
+            let nonce = Arc::new(Mutex::new(String::new()));
+            let data = Arc::new(Mutex::new(String::new()));
+            let hash = Arc::new(Mutex::new(String::new()));
+
+            let cores: u8 =
+                (((num_cpus::get() * stress.parse::<usize>().unwrap()) / 100) as f32).ceil() as u8;
+            Parallel::new()
+                .each(0..cores, |_index| {
+                    create(info, level.clone(), &nonce, &data, &hash)
+                })
+                .run();
+            let duration = start.elapsed();
+
+            println!("Cantidad de CPU usadas: {} - {}%", cores, stress);
+            println!("Nivel de dificultadad: {}", level);
+            println!("Nonce encontrado: {}", nonce.lock().unwrap());
+            println!("Hash: {}", hash.lock().unwrap());
+            println!("Datos resultantes: {}", data.lock().unwrap());
+            println!("Tiempo transcurrido: {:?}s", duration.as_secs());
+        }
+        "check" => check(
+            args[2].to_string(),
+            args[3].to_string(),
+            args[4].to_string(),
+        ),
+        _ => error_option(),
+    }
+}
+
+fn set_flag_to_true() {
+    GLOBAL_FLAG.store(true, Ordering::SeqCst);
+}
+
+fn get_flag() -> bool {
+    return GLOBAL_FLAG.load(Ordering::SeqCst);
+}
+
+fn create(
+    info: String,
+    level: String,
+    nonce_final: &Arc<Mutex<String>>,
+    data_final: &Arc<Mutex<String>>,
+    hash_final: &Arc<Mutex<String>>,
+) {
     let mut nonce = nonce_random().to_string();
-    let mut data = args[1].to_string().replace(
+    let mut data = info.replace(
         "\"nonce\": null",
         &String::from("\"nonce\": ".to_owned() + &nonce),
     );
     let mut hash = blake3(&data.as_bytes());
 
-    let pattern = format!(r"^(0){{{}}}", args[2]);
+    let pattern = format!(r"^(0){{{}}}", level);
     let regex = Regex::new(pattern.as_str()).unwrap();
     let mut option = regex.is_match(&hash.to_string());
 
-    let mut count = 0;
-    while option == false {
+    if option == true {
+        set_flag_to_true();
+        *nonce_final.lock().unwrap() = nonce;
+        *data_final.lock().unwrap() = data;
+        *hash_final.lock().unwrap() = hash.to_string();
+        return;
+    }
+
+    while option == false && get_flag() == false {
         nonce = nonce_random().to_string();
-        data = args[1].to_string().replace(
+        data = info.replace(
             "\"nonce\": null",
             &String::from("\"nonce\": ".to_owned() + &nonce),
         );
         hash = blake3(&data.as_bytes());
         option = regex.is_match(&hash.to_string());
-        count += 1;
-    }
-    let duration = start.elapsed();
 
-    println!("Nivel de dificultadad: {}", args[2]);
-    println!("Nonce encontrado: {}", nonce);
-    println!("Datos resultantes: {}", data);
-    println!("Hash: {}", hash.to_string());
-    println!("Cantidad de intentnos: {}", count.separate_with_dots());
-    println!("Tiempo transcurrido: {:?}s", duration.as_secs());
+        if option == true {
+            set_flag_to_true();
+            *nonce_final.lock().unwrap() = nonce;
+            *data_final.lock().unwrap() = data;
+            *hash_final.lock().unwrap() = hash.to_string();
+            return;
+        }
+    }
 }
 
 fn nonce_random() -> u32 {
     unsafe {
         return randombytes_random();
     }
+}
+
+fn check(info: String, nonce: String, hash: String) {
+    let data = info.replace(
+        "\"nonce\": null",
+        &String::from("\"nonce\": ".to_owned() + &nonce),
+    );
+    let hash_confirm = blake3(&data.as_bytes());
+    if hash_confirm.to_string() == hash {
+        println!("OK");
+    } else {
+        println!("ERROR");
+    }
+}
+
+fn error_option() {
+    println!("Las opciones válidas son 'create' y 'check'");
 }
